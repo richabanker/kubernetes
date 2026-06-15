04db3728936dd1335e45f11adf53a831e7962d70
Lalit Chauhan <lalitchauhan@google.com>
2026-06-15 17:51:21 +0000

    Add objectMeta validation in the path of DV testing

--- pkg/api/testing/validation_test.go
+++ pkg/api/testing/validation_test.go
@@ -103,7 +103,9 @@ func TestVersionedValidationByFuzzing(t *testing.T) {
 					if subresource != "" {
 						opts = append(opts, WithSubResources(subresource))
 					}
-
+					// extensions/v1beta1 is unserved and no longer carries
+					// declarative validation;
+					opts = append(opts, WithSkipGroupVersions("extensions/v1beta1"))
 					VerifyVersionedValidationEquivalence(t, obj, nil, opts...)
 
 					old, err := legacyscheme.Scheme.New(gv.WithKind(kind))
